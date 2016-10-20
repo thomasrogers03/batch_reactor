6d2b57dddc8609070fd78511d556be1b26685ffb
Thomas Rogers <thomasrogers03@gmail.com>
2016-10-20 08:29:44 -0400

    Refactor Retry::Policy

--- lib/batch_reactor/retry/policy.rb
+++ lib/batch_reactor/retry/policy.rb
@@ -7,25 +7,37 @@ module BatchReactor
 
     def handle_future(lock, future, batch, retry_buffer)
       future.fallback do |error|
-        work_by_status = batch.group_by do |work|
-          !!should_retry?(work, error)
-        end
-        failed_results = work_by_status[false]
-        if failed_results
-          failed_results.each do |work|
-            work.promise.fail(error)
-          end
-        end
-        successful_results = work_by_status[true]
-        if successful_results
-          lock.synchronize do
-            successful_results.each do |work|
-              work.retry_count += 1
-              retry_buffer << work
-            end
-          end
-        end
+        work_by_status = work_by_status(batch, error)
+        fail_failures!(error, work_by_status[false])
+        perform_retries!(lock, retry_buffer, work_by_status[true])
         ThomasUtils::Future.error(ERROR)
+      end
+    end
+
+    private
+
+    def work_by_status(batch, error)
+      batch.group_by do |work|
+        !!should_retry?(work, error)
+      end
+    end
+
+    def perform_retries!(lock, retry_buffer, successful_results)
+      if successful_results
+        lock.synchronize do
+          successful_results.each do |work|
+            work.retry_count += 1
+            retry_buffer << work
+          end
+        end
+      end
+    end
+
+    def fail_failures!(error, failed_results)
+      if failed_results
+        failed_results.each do |work|
+          work.promise.fail(error)
+        end
       end
     end
   end
