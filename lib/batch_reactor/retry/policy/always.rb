2ad0e77827036205b218fdae239fcefe758b77ea
Thomas Rogers <thomasrogers03@gmail.com>
2016-10-21 07:58:23 -0400

    Always retry policy now counts the number of failed batches

--- lib/batch_reactor/retry/policy/always.rb
+++ lib/batch_reactor/retry/policy/always.rb
@@ -3,13 +3,20 @@ module BatchReactor
     module Policy
       class Always
         include Policy
+        extend Forwardable
+
+        def_delegator :@retry_count, :value, :retry_count
+
+        def initialize
+          @retry_count = Concurrent::AtomicFixnum.new
+        end
 
         def should_retry?(_, _)
           true
         end
 
         def count_retry(_)
-
+          @retry_count.increment
         end
       end
     end
--- spec/classes/retry/policy/always_spec.rb
+++ spec/classes/retry/policy/always_spec.rb
@@ -7,12 +7,21 @@ module BatchReactor
 
         it { is_expected.to be_a_kind_of(Policy) }
 
-        it { is_expected.to respond_to(:count_retry).with(1).arguments }
-
         it { is_expected.to respond_to(:should_retry?).with(2).arguments }
 
         it 'should always retry' do
           expect(subject.should_retry?(nil, nil)).to eq(true)
+        end
+
+        describe '#count_retry' do
+          let(:retry_count) { rand(5..10) }
+
+          it { is_expected.to respond_to(:count_retry).with(1).arguments }
+
+          it 'should keep a count of the number of failed batches' do
+            retry_count.times { subject.count_retry(nil) }
+            expect(subject.retry_count).to eq(retry_count)
+          end
         end
 
       end
